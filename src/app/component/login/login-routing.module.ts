5a571ed6089839b9f1caf8440205ad1c32f8c9ce
technologicaljerry <cserajatverma@gmail.com>
2023-11-18 23:19:48 +0530

    Update login-routing.module.ts

--- src/app/component/login/login-routing.module.ts
+++ src/app/component/login/login-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login.component';
 
 const routes: Routes = [
-  { path: '', component: RegisterComponent, pathMatch: 'full' }
+  { path: '', component: LoginComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
