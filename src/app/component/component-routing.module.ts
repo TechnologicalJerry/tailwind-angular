0d139f73b9f86d610f192689920f00a8c9556098
technologicaljerry <cserajatverma@gmail.com>
2023-11-18 23:23:43 +0530

    Update component-routing.module.ts

--- src/app/component/component-routing.module.ts
+++ src/app/component/component-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'about',
     loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
   }
 ];
