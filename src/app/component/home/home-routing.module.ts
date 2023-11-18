aaae53c84add086fd5114de8d137563459f2b4d4
technologicaljerry <cserajatverma@gmail.com>
2023-11-18 23:20:14 +0530

    Update home-routing.module.ts

--- src/app/component/home/home-routing.module.ts
+++ src/app/component/home/home-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [
-  { path: '', component: RegisterComponent, pathMatch: 'full' }
+  { path: '', component: HomeComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
