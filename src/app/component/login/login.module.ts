e1a1705ada06e2a12930384b89f36076c83eb2b0
technologicaljerry <cserajatverma@gmail.com>
2023-11-20 23:38:38 +0530

    Update login.module.ts

--- src/app/component/login/login.module.ts
+++ src/app/component/login/login.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
-
 @NgModule({
   declarations: [
     LoginComponent
