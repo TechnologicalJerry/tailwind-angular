0bbe4a51f1d88f6299f8f0a90cd9d9cee3857871
technologicaljerry <cserajatverma@gmail.com>
2023-11-21 23:13:46 +0530

    Update component.module.ts

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentRoutingModule } from './component-routing.module';
 
-
 @NgModule({
   declarations: [],
   imports: [
